750f00b7d7de25feb539a28a4fcc2deb2310e6f3
subin1107 <btsiu@naver.com>
2022-07-01 18:31:40 +0900

    기본 환경

---
+++ src/main/java/com/error504/baf/BaFApplication.java
@@ -0,0 +1,13 @@
+package com.error504.baf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaFApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaFApplication.class, args);
+    }
+
+}
