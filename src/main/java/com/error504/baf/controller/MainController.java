369d8b2d89d588e78f37c9ab70687f41d51cac4f
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-07-24 19:00:10 +0900

    Add Navigation Bar

--- src/main/java/com/error504/baf/controller/MainController.java
+++ src/main/java/com/error504/baf/controller/MainController.java
@@ -10,4 +10,9 @@ public class MainController {
     public String index() {
         return "layout";
     }
+
+    @RequestMapping("/visitReview")
+    public String visitReview() {
+        return "visitReview";
+    }
 }
