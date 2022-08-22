de96d5f4daa2361bd9ae8b40c8443f673f4aaef4
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-08-22 17:44:04 +0900

    Update Taxi Data input

--- src/main/java/com/error504/baf/controller/TaxiController.java
+++ src/main/java/com/error504/baf/controller/TaxiController.java
@@ -2,6 +2,8 @@ package com.error504.baf.controller;
 
 import com.error504.baf.model.SiteUser;
 import com.error504.baf.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,12 +13,21 @@ import java.security.Principal;
 
 @Controller
 public class TaxiController {
-    private UserService userService;
+    private final UserService userService;
+
+    @Autowired
+    public TaxiController(UserService userService){
+        this.userService = userService;
+    }
+
+//    @PreAuthorize("isAuthenticated()")
 
     @GetMapping("/taxi")
     public String setTaxiPage(Model model, Principal principal){
-        SiteUser siteUser = userService.getUser(principal.getName());
-//        model.addAttribute("name", name);
+        if (principal != null) {
+            SiteUser siteUser = userService.getUser(principal.getName());
+            model.addAttribute("siteUser", siteUser);
+        }
 
         return "/baf_map/taxi";
     }
