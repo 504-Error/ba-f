192beb9c48cf419a6dfcb889a0b3edc2562ed7e1
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-08-26 07:54:47 +0900

    Update Image Expand Content

---
+++ src/main/java/com/error504/baf/controller/ErrorController.java
@@ -0,0 +1,11 @@
+package com.error504.baf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("${server.error.path:${error.path:/error}}")
+public class ErrorController {
+
+
+}
