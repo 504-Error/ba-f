a60fdfb3ec921955ab63ff572e1129e461d026f3
JOIKJ <cbqnk9@gmail.com>
2022-09-14 23:16:36 +0900

    아이콘 안 뜨는 에러 수정

--- src/main/resources/static/script/icon_map.js
+++ src/main/resources/static/script/icon_map.js
@@ -6,7 +6,7 @@ var placeOverlay = new kakao.maps.CustomOverlay({zIndex:1}),
 
 var mapContainer = document.getElementById('map'), // 지도를 표시할 divg
     mapOption = {
-        center: new kakao.maps.LatLng(37.566826, 126.9786567), // 지도의 중심좌표
+        center: new kakao.maps.LatLng(37.557694280190645,126.92660457775837), // 지도의 중심좌표
         level: 3 // 지도의 확대 레벨
     };
 
--- src/main/resources/static/script/search.js
+++ src/main/resources/static/script/search.js
@@ -110,9 +110,35 @@ function displayPlaces(places) {
         // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
         // LatLngBounds 객체에 좌표를 추가합니다
         bounds.extend(placePosition);
+        // 마커와 검색결과 항목에 mouseover 했을때
+        // 해당 장소에 인포윈도우에 장소명을 표시합니다
+        // mouseout 했을 때는 인포윈도우를 닫습니다
+        (function(marker, title) {
+            kakao.maps.event.addListener(marker, 'mouseover', function() {
+                displayInfowindow(marker, title);
+            });
+
+            kakao.maps.event.addListener(marker, 'mouseout', function() {
+                infowindow.close();
+            });
+
+            itemEl.onmouseover =  function () {
+                displayInfowindow(marker, title);
+            };
+
+            itemEl.onmouseout =  function () {
+                infowindow.close();
+            };
+        })(marker, places[i].place_name);
         fragment.appendChild(itemEl)
+    }
+// 검색결과 목록 또는 마커를 클릭했을 때 호출되는 함수입니다
+// 인포윈도우에 장소명을 표시합니다
+    function displayInfowindow(marker, title) {
+        var content = '<div style="padding:5px;z-index:1;">' + title + '</div>';
 
-
+        infowindow.setContent(content);
+        infowindow.open(map, marker);
     }
 
     // 검색결과 항목들을 검색결과 목록 Element에 추가합니다
